9ad0ea0f7851d34d7e8d3374bb5fb2ee342df6f0
darkglimmer <2392897209@qq.com>
2020-06-10 15:01:34 +0800

    feat: change api

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://119.23.79.87:1999/',
+                target: 'http://116.62.7.230:1999/',
                 changeOrigin: true,
                 ws: true,
                 pathRewrite: {
